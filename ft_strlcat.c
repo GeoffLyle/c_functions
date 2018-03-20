cef72f196e4c38d487baf6b151527ce203443b6a
Alfred Lyle <alyle@e1z1r4p36.42.us.org>
2018-03-19 23:24:06 -0700

    Add my versions of strlcat and strlcpy

---
+++ ft_strlcat.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alyle <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/03/19 20:41:39 by alyle             #+#    #+#             */
+/*   Updated: 2018/03/19 21:59:34 by alyle            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlen(char *str)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	j;
+	unsigned int	destlen;
+	unsigned int	srclen;
+
+	j = 0;
+	destlen = ft_strlen(dest);
+	srclen = ft_strlen(src);
+	i = destlen;
+	if (destlen >= size)
+		return (srclen + size);
+	while (src[j] != '\0' && i + j < size - 1)
+	{
+		dest[i + j] = src[j];
+		j++;
+	}
+	dest[i + j] = '\0';
+	return (destlen + srclen);
+}
---
+++ ft_strlcpy.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alyle <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/03/19 21:35:46 by alyle             #+#    #+#             */
+/*   Updated: 2018/03/19 23:21:15 by alyle            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlen(char *str)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	destlen;
+	unsigned int	srclen;
+
+	i = 0;
+	destlen = ft_strlen(dest);
+	srclen = ft_strlen(src);
+	if (size == 0)
+		return (srclen);
+	while (src[i] != '\0' && i < size - 1 && i < destlen - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (srclen);
+}
