0b7940e3fcfd79971022d5fc9d13415c299ae84f
Alfred Lyle <alyle@e1z1r4p36.42.us.org>
2018-03-20 19:28:03 -0700

    Use string pointers

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: alyle <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/19 21:35:46 by alyle             #+#    #+#             */
-/*   Updated: 2018/03/19 23:21:15 by alyle            ###   ########.fr       */
+/*   Updated: 2018/03/20 19:18:04 by alyle            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@ unsigned int	ft_strlen(char *str)
 	unsigned int	i;
 
 	i = 0;
-	while (str[i] != '\0')
+	while (*str++ != '\0')
 	{
 		i++;
 	}
@@ -24,20 +24,20 @@ unsigned int	ft_strlen(char *str)
 
 unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 {
-	unsigned int	i;
 	unsigned int	destlen;
 	unsigned int	srclen;
 
-	i = 0;
 	destlen = ft_strlen(dest);
 	srclen = ft_strlen(src);
 	if (size == 0)
 		return (srclen);
-	while (src[i] != '\0' && i < size - 1 && i < destlen - 1)
+	while (*src != '\0' && size > 1)
 	{
-		dest[i] = src[i];
-		i++;
+		*dest = *src;
+		dest++;
+		src++;
+		size--;
 	}
-	dest[i] = '\0';
+	*dest = '\0';
 	return (srclen);
 }
