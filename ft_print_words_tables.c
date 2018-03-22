451bce88220149bfa40c116196f54f525f0de5c0
Alfred Lyle <geofflyle@gmail.com>
2018-03-21 23:01:16 -0700

    Add ability to split string by whitespace and output the result

---
+++ ft_print_words_tables.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_words_tables.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alyle <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/03/21 17:20:19 by alyle             #+#    #+#             */
+/*   Updated: 2018/03/21 20:54:08 by alyle            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+void	ft_print_words_tables(char **tab)
+{
+	int		i;
+
+	i = 0;
+	while (tab[i])
+	{
+		ft_putstr(tab[i]);
+		ft_putchar('\n');
+		i++;
+	}
+}
---
+++ ft_split_whitespaces.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split_whitespaces.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alyle <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/03/21 15:42:34 by alyle             #+#    #+#             */
+/*   Updated: 2018/03/21 18:11:30 by alyle            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int		ft_copy_word(char *dest, char *src, int j)
+{
+	int		i;
+
+	i = 0;
+	while (src[j] != '\0' && src[j] != ' ' && src[j] != '\n' && src[j] != '\t')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (j);
+}
+
+int		ft_strlen(char *str)
+{
+	int		i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	**ft_split_whitespaces(char *str)
+{
+	char	**strarray;
+	int		word;
+	int		j;
+	int		strlen;
+
+	strlen = ft_strlen(str);
+	strarray = malloc(strlen * sizeof(char*));
+	word = 0;
+	j = 0;
+	while (str[j])
+	{
+		if (str[j] == ' ' || str[j] == '\n' || str[j] == '\t')
+			j++;
+		else
+		{
+			strarray[word] = (char*)malloc(sizeof(char*) * (strlen - j));
+			j = ft_copy_word(strarray[word], str, j);
+			word++;
+		}
+	}
+	strarray[word] = 0;
+	return (strarray);
+}
