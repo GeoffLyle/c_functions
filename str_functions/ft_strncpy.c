0b764a3e4e9d0212723ba2ada1c1f273eadc3871
Alfred Lyle <geofflyle@gmail.com>
2018-03-26 15:42:13 -0700

    Modify strncpy to always fill remaining spaces with NUL

--- str_functions/ft_strncpy.c
+++ str_functions/ft_strncpy.c
@@ -6,37 +6,23 @@
 /*   By: alyle <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/19 12:16:43 by alyle             #+#    #+#             */
-/*   Updated: 2018/03/19 15:12:14 by alyle            ###   ########.fr       */
+/*   Updated: 2018/03/26 15:40:49 by alyle            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int		ft_strlen(char *str)
-{
-	int		i;
-
-	i = 0;
-	while (str[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
-}
-
 char	*ft_strncpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int		i;
-	unsigned int		srclen;
-	unsigned int		destlen;
+	unsigned int	i;
 
 	i = 0;
-	srclen = ft_strlen(src);
-	destlen = ft_strlen(dest);
-	while (i < n && i < destlen + 1)
+	while (src[i] && i < n)
 	{
-		if (i < srclen)
-			dest[i] = src[i];
-		else
-			dest[i] = '\0';
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
 		i++;
 	}
 	return (dest);
