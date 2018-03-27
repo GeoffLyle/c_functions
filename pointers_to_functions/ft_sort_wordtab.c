36ae7b5451401a18486c12b5b026af5e423f4a01
Alfred Lyle <geofflyle@gmail.com>
2018-03-26 23:25:54 -0700

    Streamline to place bubble sort function inside of ft_swort_wordtab function

--- pointers_to_functions/ft_sort_wordtab.c
+++ pointers_to_functions/ft_sort_wordtab.c
@@ -6,7 +6,7 @@
 /*   By: alyle <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/26 21:03:37 by alyle             #+#    #+#             */
-/*   Updated: 2018/03/26 21:19:05 by alyle            ###   ########.fr       */
+/*   Updated: 2018/03/26 23:23:07 by alyle            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,35 +28,29 @@ int		ft_strcmp(char *s1, char *s2)
 	return (s1[i]);
 }
 
-void	ft_bubblesort(int argc, char **argv)
-{
-	int		j;
-	char	*tmp;
-
-	j = 0;
-	while (argc > 0)
-	{
-		while (j < argc - 1)
-		{
-			if (ft_strcmp(argv[j], argv[j + 1]) > 0)
-			{
-				tmp = argv[j];
-				argv[j] = argv[j + 1];
-				argv[j + 1] = tmp;
-			}
-			j++;
-		}
-		j = 0;
-		argc--;
-	}
-}
-
 void	ft_sort_wordtab(char **tab)
 {
 	int		i;
+	int		j;
+	char	*tmp;
 
 	i = 0;
 	while (tab[i])
 		i++;
-	return (ft_bubblesort(i, tab));
+	j = 0;
+	while (i > 0)
+	{
+		while (j < i - 1)
+		{
+			if (ft_strcmp(tab[j], tab[j + 1]) > 0)
+			{
+				tmp = tab[j];
+				tab[j] = tab[j + 1];
+				tab[j + 1] = tmp;
+			}
+			j++;
+		}
+		j = 0;
+		i--;
+	}
 }
