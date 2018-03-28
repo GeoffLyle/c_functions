58880c5af54c90b5d1351340286f16d83d42f1bb
Alfred Lyle <geofflyle@gmail.com>
2018-03-27 22:14:04 -0700

    Add ability to add elements to the front of a linked list

---
+++ linked_lists/ft_list_push_front.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_push_front.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alyle <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/03/27 15:37:05 by alyle             #+#    #+#             */
+/*   Updated: 2018/03/27 15:44:32 by alyle            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+
+void	ft_list_push_front(t_list **begin_list, void *data)
+{
+	t_list	*new;
+
+	if (*begin_list)
+	{
+		new = ft_create_elem(data);
+		new->next = *begin_list;
+		*begin_list = new;
+	}
+	else
+	{
+		*begin_list = ft_create_elem(data);
+	}
+}
