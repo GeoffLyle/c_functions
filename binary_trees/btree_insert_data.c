259cdadf9c9914d64e5c386ae631fa7d2be49c46
Alfred Lyle <geofflyle@gmail.com>
2018-03-30 22:43:29 -0700

    Add function to add elements to binary tree in the correct position

---
+++ binary_trees/btree_insert_data.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   btree_insert_data.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alyle <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/03/30 14:11:57 by alyle             #+#    #+#             */
+/*   Updated: 2018/03/30 14:53:27 by alyle            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_btree.h"
+
+void	btree_insert_data(t_btree **root, void *item,
+		int (*cmpf)(void *, void *))
+{
+	if (!*root)
+		*root = btree_create_node(item);
+	else
+	{
+		if (cmpf((*root)->item, item) > 0)
+			btree_insert_data(&(*root)->left, item, cmpf);
+		else
+			btree_insert_data(&(*root)->right, item, cmpf);
+	}
+}
